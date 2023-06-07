d79df817d54ebd2a5f871f460a217a0ae7fa4b72
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-07 22:28:57 +0200

    fix github link

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-    // Your site's detail?
-    name: 'Jimy Marletta',
-    title: 'Portfolio de Jimy Marletta',
-    description: 'Développeur full stack et no/low-code',
-    url: 'https://astro-me.vercel.app',
-    githubUrl: 'https://github.com/jimymltta'
-}
+  // Your site's detail?
+  name: 'Jimy Marletta',
+  title: 'Portfolio de Jimy Marletta',
+  description: 'Développeur full stack et no/low-code',
+  url: 'https://astro-me.vercel.app',
+  githubUrl: 'https://github.com/honocoder',
+};
